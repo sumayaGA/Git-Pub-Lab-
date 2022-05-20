3b79b13c8bfe1233f6417e943da695f13ba537c0
Sumaya Ali <sumayaali@Sumayas-iMac.local>
2022-05-20 12:55:50 -0500

    Sets up index view to show drinksdata

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require ("express");
+const express = require("express");
 
-const app = express (); 
+const app = express(); 
 
 const port = 3000;
 
-const drinks = require ("./models/drinks");
+const drinks = require("./models/drinks");
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!");
 });
 
-app.get ("/drinks", (req, res) => {
-    res.render ("drinks_index.ejs");
+app.get("/drinks", (req,res) => {
+    res.render("drinks_index.ejs", {allDrinks:drinks});
 });
+
+// app.get ("/drinks/:index", (req, res) => {
+//     res.render ("drinks_index.ejs",  {
+//         drink:drinks[req.params.index],
+//     });
+// });
 
 
 app.listen(port, ()=> {
