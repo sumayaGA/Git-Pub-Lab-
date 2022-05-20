3e79e3b56de421dcb9d4e020818dcfa6d583947c
Sumaya Ali <sumayaali@Sumayas-iMac.local>
2022-05-20 13:20:37 -0500

    Sets up show route

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const port = 3000;
 
 const drinks = require("./models/drinks");
 
-
+//Tests that port is functioning
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!");
 });
 
+//Index
 app.get("/drinks", (req,res) => {
     res.render("drinks_index.ejs", {allDrinks:drinks});
 });
+
 
 // app.get ("/drinks/:index", (req, res) => {
 //     res.render ("drinks_index.ejs",  {
@@ -21,6 +23,10 @@ app.get("/drinks", (req,res) => {
 //     });
 // });
 
+//Show
+app.get("/drinks/:id",(req,res)=>{
+    res.send(req.params.id);
+});
 
 app.listen(port, ()=> {
     console.log("Listening from port ", port);
