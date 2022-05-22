c681f7b60cfbba1a8999cb3b1d6510910f55dc7e
Sumaya Ali <sumayaali@Sumayas-iMac.local>
2022-05-22 09:30:21 -0500

    final commit

--- server.js
+++ server.js
@@ -5,30 +5,34 @@ const app = express();
 const port = 3000;
 
 const drinks = require("./models/drinks");
+const food = require("./models/food");
 
-//Tests that port is functioning
+//Root Index
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!");
 });
 
-//Index
+//Drinks Index && Food Index
 app.get("/drinks", (req,res) => {
-    res.render("drinks_index.ejs", {allDrinks:drinks});
+    res.render("drinks_index.ejs", {allDrinks:drinks, allFood:food});
 });
 
 
-// app.get ("/drinks/:index", (req, res) => {
-//     res.render ("drinks_index.ejs",  {
-//         drink:drinks[req.params.index],
-//     });
-// });
 
-//Show
+
+
+//Show Drinks Route
 app.get("/drinks/:id",(req,res)=>{
     res.render("drinks_show.ejs", {allDrinks:drinks});
 });
 
+// //Show Food Route
+app.get("/food/:id",(req,res)=>{
+    res.render("food_show.ejs", {allFood:food});
+});
 
+
+//Listen for the port
 app.listen(port, ()=> {
     console.log("Listening from port ", port);
 }); 
